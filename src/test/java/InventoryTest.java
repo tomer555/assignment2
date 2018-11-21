89816ee958d7a7bc068bc80db22636437c8f1aac
tomer555 <tomer19611@gmail.com>
2018-11-21 20:09:21 +0200

    Initial commit

---
+++ src/test/java/InventoryTest.java
@@ -0,0 +1,28 @@
+package java;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class InventoryTest {
+
+    @Test
+    public void getInstance() {
+    }
+
+    @Test
+    public void load() {
+    }
+
+    @Test
+    public void take() {
+    }
+
+    @Test
+    public void checkAvailabiltyAndGetPrice() {
+    }
+
+    @Test
+    public void printInventoryToFile() {
+    }
+}
---
+++ src/test/java/MessageBusTest.java
@@ -0,0 +1,40 @@
+package java;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MessageBusTest {
+
+    @Test
+    public void subscribeEvent() {
+    }
+
+    @Test
+    public void subscribeBroadcast() {
+    }
+
+    @Test
+    public void complete() {
+    }
+
+    @Test
+    public void sendBroadcast() {
+    }
+
+    @Test
+    public void sendEvent() {
+    }
+
+    @Test
+    public void register() {
+    }
+
+    @Test
+    public void unregister() {
+    }
+
+    @Test
+    public void awaitMessage() {
+    }
+}
