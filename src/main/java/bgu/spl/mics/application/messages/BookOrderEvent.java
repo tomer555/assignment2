7fa046a637dc907901d5cef929e3ef5533665abe
tomer555 <tomer19611@gmail.com>
2018-11-27 00:18:54 +0200

    added classes

---
+++ src/main/java/bgu/spl/mics/application/messages/BookOrderEvent.java
@@ -0,0 +1,15 @@
+package bgu.spl.mics.application.messages;
+import bgu.spl.mics.Event;
+import bgu.spl.mics.application.passiveObjects.Customer;
+import bgu.spl.mics.application.passiveObjects.OrderReceipt;
+
+public class BookOrderEvent implements Event<OrderReceipt> {
+    private String senderName;
+    BookOrderEvent(String senderName, Customer customer){
+        this.senderName=senderName;
+
+    }
+
+
+
+}
